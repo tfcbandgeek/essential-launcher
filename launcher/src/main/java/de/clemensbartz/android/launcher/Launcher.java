741861718564dfe7c0c80093219fcbed7848b3ea
Clemens Bartz <clemens@clemens-bartz.de>
2018-07-07 12:03:06 +0200

    Optimize imports.

--- launcher/src/main/java/de/clemensbartz/android/launcher/Launcher.java
+++ launcher/src/main/java/de/clemensbartz/android/launcher/Launcher.java
@@ -42,7 +42,6 @@ import android.os.Parcelable;
 import android.os.StrictMode;
 import android.util.Pair;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
